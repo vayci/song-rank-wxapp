1e5264bea4aaf6ae47dc07b04fcce6d674ab8031
vayci <vayci2012@163.com>
2019-03-06 12:28:33 +0800

    note undefined

--- pages/note/note.js
+++ pages/note/note.js
@@ -1,6 +1,9 @@
 Page({
   jump:function(param){
     //console.log(param.target.dataset.index)
+    if (param.target.dataset.index == undefined){
+      return;
+    }
     wx.navigateTo({
       url: '/pages/note-detail/note-detail?index=' + param.target.dataset.index
         + '&title=' + param.target.dataset.title
