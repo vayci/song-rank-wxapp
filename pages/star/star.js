870cac4a6d4d0bfb6db63a49fab074a02655ea8e
vayci <vayci2012@163.com>
2019-05-10 21:41:16 +0800

    统计页面

--- pages/star/star.js
+++ pages/star/star.js
@@ -1,23 +1,49 @@
-// pages/star/star.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
-   */
+const app = getApp()
+Page({
   data: {
-
+    proxyCount: 0,
+    dailyCount:0,
+    top:[],
+    latest:[],
+    timer:null
   },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
+  onLoad(options) {
+  },
+  onHide() {
+    console.log('清理定时任务'+this.timer)
+    clearInterval(this.timer)
+  },
+  onShow() {
+    let _this = this
+    this.getReport()
+    let num = setInterval(function () {
+      _this.getReport()
+    }, 5000)
+    this.timer = num
+    console.log('创建定时任务'+this.timer)
+  },
+  onPullDownRefresh: function () {
+    this.getReport()
+  },
+  getReport() {
+    let _this = this;
+    wx.request({
+      url: app.globalData.serverUrl + '/health/report/info',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        let data = res.data
+        _this.setData({
+          proxyCount: data.proxyCount,
+          dailyCount: data.dailyCount,
+          top: data.top,
+          latest:data.latest
+        })
+      }
+    })
+  },
+  data: {
+    userList: []
   }
 })
