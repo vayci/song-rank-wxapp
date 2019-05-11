1d1a64b26229062bc20b8088c3f9f5822c4bd4ed
vayci <vayci2012@163.com>
2019-05-11 13:54:34 +0800

    统计页面

--- pages/star/star.js
+++ pages/star/star.js
@@ -1,11 +1,14 @@
 const app = getApp()
+const utils = require('../../utils/util.js')
 Page({
   data: {
     proxyCount: 0,
+    proxyText:'',
     dailyCount:0,
     top:[],
     latest:[],
-    timer:null
+    timer:null,
+    proxyColor:'red'
   },
   onLoad(options) {
   },
@@ -22,9 +25,6 @@ Page({
     this.timer = num
     console.log('创建定时任务'+this.timer)
   },
-  onPullDownRefresh: function () {
-    this.getReport()
-  },
   getReport() {
     let _this = this;
     wx.request({
@@ -38,10 +38,47 @@ Page({
           proxyCount: data.proxyCount,
           dailyCount: data.dailyCount,
           top: data.top,
-          latest:data.latest
+          latest: _this.parseLatest(data.latest),
+          proxyText: _this.getStatus(data.proxyCount),
+          proxyColor: _this.getColor(data.proxyCount)
         })
       }
     })
+  },
+  parseLatest(latest){
+    var now = new Date();
+    latest.forEach(function (value, i) {
+      let changetime = utils.str2Date(value.changeTime)
+      let sub = (now.getTime()-changetime.getTime())/1000
+      if (sub < 60)
+        value.changeTime = parseInt(sub) + '秒'
+      if (sub>=60){
+        let min = parseInt(sub/60)
+        let s = parseInt(sub%60)
+        value.changeTime = min + '分' + s + '秒'
+      }
+    })
+    return latest;
+  },
+  getStatus(count){
+    if (count == 0) 
+    return '极差'
+    if (count <= 3)
+    return'较差'
+    if (count > 3 && count <= 10) 
+    return '一般'
+    if (count > 10 && count <= 20) 
+    return '良好'
+    if (count > 20) 
+    return '极好'
+  },
+  getColor(count){
+    if (count == 0)
+      return '#fe5151'
+    if (count <= 3)
+      return '#ffd153'
+    if (count > 3 && count <= 10)
+      return '#45ce90'
   },
   data: {
     userList: []
