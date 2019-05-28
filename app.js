0fe429967b2547625acf3aafe4cffbe72064bb37
vayci <vayci2012@163.com>
2019-05-28 15:16:49 +0800

    app.js

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
-    // 登录
     wx.login({
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
         this.globalData.code = res.code;
         if (this.codeReadyCallback) {
           this.codeReadyCallback(res)
@@ -36,11 +30,6 @@ App({
         console.log("SDKVersion: "+res.SDKVersion);
       }
     })
-    // 查看用户是否授权
-    // wx.getSetting({
-    //   success: res => {
-    //   }
-    // })
   },
   globalData: {
     userInfo: null,
